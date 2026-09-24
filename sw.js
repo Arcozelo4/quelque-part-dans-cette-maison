ca42bdca77f675897d30a99fb4c73672cad3f1e0
Arcozelo4 <heidelbergerjessy@hotmail.fr>
2026-09-24 16:11:17 +0200

    Add service worker for caching files

---
+++ sw.js
@@ -0,0 +1,102 @@
+const CACHE_NAME =
+  "quelque-part-maison-v1";
+
+const FICHIERS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon.svg"
+];
+
+self.addEventListener(
+  "install",
+  event => {
+    event.waitUntil(
+      caches
+        .open(CACHE_NAME)
+        .then(cache =>
+          cache.addAll(FICHIERS)
+        )
+    );
+
+    self.skipWaiting();
+  }
+);
+
+self.addEventListener(
+  "activate",
+  event => {
+    event.waitUntil(
+      caches
+        .keys()
+        .then(noms =>
+          Promise.all(
+            noms
+              .filter(
+                nom =>
+                  nom !== CACHE_NAME
+              )
+              .map(
+                nom =>
+                  caches.delete(nom)
+              )
+          )
+        )
+    );
+
+    self.clients.claim();
+  }
+);
+
+self.addEventListener(
+  "fetch",
+  event => {
+    if (
+      event.request.method !== "GET"
+    ) {
+      return;
+    }
+
+    event.respondWith(
+      caches
+        .match(event.request)
+        .then(
+          reponseCache => {
+            if (reponseCache) {
+              return reponseCache;
+            }
+
+            return fetch(
+              event.request
+            )
+              .then(
+                reponse => {
+                  const copie =
+                    reponse.clone();
+
+                  caches
+                    .open(CACHE_NAME)
+                    .then(
+                      cache =>
+                        cache.put(
+                          event.request,
+                          copie
+                        )
+                    );
+
+                  return reponse;
+                }
+              )
+              .catch(
+                () =>
+                  caches.match(
+                    "./index.html"
+                  )
+              );
+          }
+        )
+    );
+  }
+);
